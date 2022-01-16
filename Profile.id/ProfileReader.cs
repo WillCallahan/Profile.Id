228178c73f3adea8212750a1ceb4c7de665098f2
WillCallahan <willcallahan94@att.net>
2022-01-16 10:48:05 -0700

    Initial commit

---
+++ Profile.id/ProfileReader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profile.id
+{
+    public static class ProfileReader
+    {
+        public const string DefaultCredentialsFilePath = ".aws\\credentials";
+
+        private static string GetDefaultCredentialsFilePath()
+        {
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            return $"{path}\\{DefaultCredentialsFilePath}";
+        }
+
+        public static List<string> GetAllProfiles(string credentialsFilePath = null)
+        {
+            if (credentialsFilePath == null)
+            {
+                credentialsFilePath = GetDefaultCredentialsFilePath();
+            }
+            var profiles = new List<string>();
+            var lines = File.ReadAllLines(credentialsFilePath);
+            foreach (var line in lines)
+            {
+                var cleanLine = line.Trim();
+                if (cleanLine.StartsWith("["))
+                {
+                    var profileName = cleanLine.Substring(1, cleanLine.Length - 2);
+                    profiles.Add(profileName);
+                }
+            }
+
+            return profiles;
+        }
+    }
+}
---
+++ Profile.id/TrayIconController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Profile.id.Properties;
+
+namespace Profile.id
+{
+    public class TrayIconController : ApplicationContext
+    {
+        private readonly NotifyIcon _trayIcon;
+
+        private List<Task> _tasks = new List<Task>();
+
+        public const string AwsProfileEnvironmentVariable = "AWS_PROFILE";
+
+        public TrayIconController()
+        {
+            var (target, activeProfileName) = GetActiveProfile();
+            var tooltip = GetActiveProfileTooltip(target, activeProfileName);
+
+            var contextMenu = GetContextMenu(activeProfileName);
+            
+
+            _trayIcon = new NotifyIcon
+            {
+                Icon = Resources.AwsIcon,
+                ContextMenu = contextMenu,
+                BalloonTipText = tooltip,
+                Text = tooltip,
+                Visible = true
+            };
+
+            _trayIcon.MouseMove += MouseHover;
+            _trayIcon.MouseDown += MouseClick;
+        }
+
+        public void Exit(object sender, EventArgs e)
+        {
+            _trayIcon.Visible = false;
+            Task.WaitAll(_tasks.ToArray());
+            Application.Exit();
+        }
+
+        public void ChangeProfile(object sender, EventArgs e)
+        {
+            if (sender is MenuItem menuItem)
+            {
+                var newProfileName = menuItem.Text;
+                var task = Task.Run(() => Environment.SetEnvironmentVariable(AwsProfileEnvironmentVariable, newProfileName, EnvironmentVariableTarget.User));
+                _tasks.Add(task);
+            }
+        }
+
+        public void MouseHover(object sender, MouseEventArgs e)
+        {
+            var (target, profileName) = GetActiveProfile();
+            var tooltip = GetActiveProfileTooltip(target, profileName);
+            if (_trayIcon.BalloonTipText == tooltip || _trayIcon.Text == tooltip)
+            {
+                return;
+            }
+            _trayIcon.BalloonTipText = tooltip;
+            _trayIcon.Text = tooltip;
+        }
+
+        public void MouseClick(object sender, MouseEventArgs e)
+        {
+            var (target, profileName) = GetActiveProfile();
+            foreach (MenuItem menuItem in _trayIcon.ContextMenu.MenuItems)
+            {
+                menuItem.Checked = profileName == menuItem.Text;
+            }
+        }
+
+        private ContextMenu GetContextMenu(string activeProfileName)
+        {
+            var profiles = ProfileReader.GetAllProfiles();
+
+            var menuItems = new List<MenuItem>();
+            var profileItems = profiles
+                .Select(p =>
+                {
+                    var menuItem = new MenuItem(p, ChangeProfile)
+                    {
+                        Checked = activeProfileName == p
+                    };
+                    return menuItem;
+                })
+                .ToList();
+
+            menuItems.AddRange(profileItems);
+            menuItems.Add(new MenuItem("-"));
+            menuItems.Add(new MenuItem("Exit", Exit));
+
+            var contextMenu = new ContextMenu(menuItems.ToArray());
+
+            return contextMenu;
+        }
+
+        private static string GetActiveProfileTooltip(EnvironmentVariableTarget? target, string profileName)
+        {
+            switch (target)
+            {
+                case EnvironmentVariableTarget.User:
+                    return $"User: {profileName}";
+                case EnvironmentVariableTarget.Machine:
+                    return $"Machine: {profileName}";
+                case EnvironmentVariableTarget.Process:
+                    return $"Process: {profileName}";
+                default:
+                    return "No Active Profile";
+            }
+        }
+
+        private static (EnvironmentVariableTarget?, string) GetActiveProfile()
+        {
+            var processProfileName = Environment.GetEnvironmentVariable(AwsProfileEnvironmentVariable, EnvironmentVariableTarget.Process);
+            if (!string.IsNullOrEmpty(processProfileName))
+            {
+                return (EnvironmentVariableTarget.Process, processProfileName);
+            }
+
+            var userProfileName = Environment.GetEnvironmentVariable(AwsProfileEnvironmentVariable, EnvironmentVariableTarget.User);
+            if (!string.IsNullOrEmpty(userProfileName))
+            {
+                return (EnvironmentVariableTarget.User, userProfileName);
+            }
+
+            var machineProfileName = Environment.GetEnvironmentVariable(AwsProfileEnvironmentVariable, EnvironmentVariableTarget.Machine);
+            if (!string.IsNullOrEmpty(machineProfileName))
+            {
+                return (EnvironmentVariableTarget.Machine, machineProfileName);
+            }
+
+            return (null, null);
+        }
+    }
+}
